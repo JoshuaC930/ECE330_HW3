18f474ff83a0e6a0eefbfb790064516df39f0e3c
Joshua Corrales <89368536+JoshuaC930@users.noreply.github.com>
2022-10-04 11:39:44 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Homework 3: Writing in C
+Repo for creating HW3 for ECE330 - Microprocessors
---
+++ TemperatureConversion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * File: Temperature Conversion, converts 
+ */
+
+int main() {
+    float centigrade, fahrenheit;
+
+    printf("Enter a number in Centigrade to convert: ");
+
+    //reads input from user
+    scanf("%d", &centigrade);
+
+    //temperature conversion
+    
+
+    return 0; //terminates program
+}
